ef4b42c1f7ee20f44bdb6c13d67eddecf9c030a3
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2025-05-02 18:56:01 +0200

    wip

--- src/components/astro.js
+++ src/components/astro.js
@@ -1,2 +1,3 @@
 export { default as Information } from "./astro/Information.astro"
+export { default as InlineSpoiler } from "./astro/InlineSpoiler.astro"
 export { default as RemoteSVG } from "./astro/RemoteSVG.astro"
